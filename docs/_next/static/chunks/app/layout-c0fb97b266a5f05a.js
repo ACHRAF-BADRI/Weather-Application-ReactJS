7a478f6bb8f2b61cd233e5fd62b9f1fe1195149b
ACHRAF EL BADRI <www.achrafweb@gmail.com>
2024-10-24 10:55:06 +0200

    upd

---
+++ docs/_next/static/chunks/app/layout-c0fb97b266a5f05a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1574:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,1034,23)),Promise.resolve().then(l.t.bind(l,3275,23))},3275:()=>{},1034:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_876593"}}},e=>{var s=s=>e(e.s=s);e.O(0,[713,130,215,744],()=>s(1574)),_N_E=e.O()}]);
